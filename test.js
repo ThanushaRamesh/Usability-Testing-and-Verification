300f16f0ad5baf5e847fdb331a700d41afcaba9f
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2020-02-20 01:38:42 +0100

    chanes in branch two

--- test.js
+++ test.js
@@ -4,3 +4,6 @@ console.log("Let the Git journey begin!!");
 //after the change I push the code to the branch_one
 
 console.log("Hello Branch one :|");
+
+// working on brach_two
+console.log("Hello Branch two :|");
